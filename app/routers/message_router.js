136969c415d2fdbee746bb021316bdca0872d9e7
Wrendra57 <89427507+Wrendra57@users.noreply.github.com>
2024-07-29 20:19:13 +0700

    w

--- app/routers/message_router.js
+++ app/routers/message_router.js
@@ -5,6 +5,9 @@ const {
 } = require("../controllers/message_controller");
 const MessageRouter = Router();
 
+MessageRouter.get("/", (req, res) => {
+  res.send("Welcome to the message router");
+});
 MessageRouter.all("/send-message", sendMessage);
 MessageRouter.all("/send-bulk-message", sendBulkMessage);
 
